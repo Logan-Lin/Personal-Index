700ceb978a445d4599235112205184ba77a1a0cc
Logan Lin <ylincs@bjtu.edu.cn>
2019-10-10 21:51:23 +0800

    Fixed a bug in night mode switching.

--- index_app/static/js/index.js
+++ index_app/static/js/index.js
@@ -9,13 +9,11 @@ $('#style-switch-btn').on('click', function() {
         $('.mdui-card').removeClass('mdui-color-grey-800');
         $('body').removeClass('mdui-color-grey-900');
         $('header').removeClass('mdui-color-black');
-        $('.mdui-fab').removeClass('mdui-color-brown-900');
         isDarkMode = false;
     } else {
         $('.mdui-card').addClass('mdui-color-grey-800');
         $('body').addClass('mdui-color-grey-900');
         $('header').addClass('mdui-color-black');
-        $('.mdui-fab').addClass('mdui-color-brown-800');
         isDarkMode = true;
     }
 })
